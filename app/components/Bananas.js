7a51f6d22e937bc3da6fc0159d5b134418f5cc0a
jbarroso <joselele@casa.micasa>
2016-09-24 09:50:31 +0200

    Adding Bananas

---
+++ app/components/Bananas.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+
+class Bananas extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
+      <Image source={pic} style={{width: 193, height: 110}}/>
+    );
+  }
+}
+
+export default Bananas;
--- app/containers/App.js
+++ app/containers/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import HelloWorld from '../components/HelloWorld';
+import Bananas from '../components/Bananas';
 
 class App extends Component {
   render() {
     return (
-      <HelloWorld />
+      <Bananas/>
     );
   }
 }
