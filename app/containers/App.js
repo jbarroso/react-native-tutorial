232a0f6ffd152e52fbe5907b0badc1a02e99f56b
jbarroso <joselele@casa.micasa>
2016-09-24 09:57:45 +0200

    Adding Blinks

--- app/containers/App.js
+++ app/containers/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import HelloWorld from '../components/HelloWorld';
 import Bananas from '../components/Bananas';
 import LotsOfGreetings from '../components/LotsOfGreetings';
+import Blinks from '../components/Blinks';
 
 class App extends Component {
   render() {
     return (
-      <LotsOfGreetings/>
+      <Blinks/>
     );
   }
 }
