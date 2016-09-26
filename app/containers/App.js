fa8d231adb690b28d9fc1f4c604c90cb09c65d70
jbarroso <joselele@casa.micasa>
2016-09-26 22:57:24 +0200

    Adding FlexDirectionBAsics, JustifyContentBasics and AlignItemsBasics

--- app/containers/App.js
+++ app/containers/App.js
@@ -6,11 +6,14 @@ import Blinks from '../components/Blinks';
 import LotsOfStyles from '../components/LotsOfStyles';
 import FixedDimensionsBasics from '../components/FixedDimensionsBasics';
 import FlexDimensionsBasics from '../components/FlexDimensionsBasics';
+import FlexDirectionBasics from '../components/FlexDirectionBasics';
+import JustifyContentBasics from '../components/JustifyContentBasics';
+import AlignItemsBasics from '../components/AlignItemsBasics';
 
 export default class App extends Component {
   render() {
     return (
-      <FlexDimensionsBasics/>
+      <AlignItemsBasics/>
     );
   }
 }
