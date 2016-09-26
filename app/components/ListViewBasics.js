195fb4560ead93a002434a58fb742035fc993869
jbarroso <joselele@casa.micasa>
2016-09-27 00:10:19 +0200

    Adding ListView

---
+++ app/components/ListViewBasics.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { ListView, Text, View } from 'react-native';
+
+export default class ListViewBasics extends Component {
+  // Initialize the hardcoded data
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([
+        'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+      ])
+    };
+  }
+  render() {
+    return (
+      <View style={{paddingTop: 22}}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) => <Text>{rowData}</Text>}
+        />
+      </View>
+    );
+  }
+}
--- app/containers/App.js
+++ app/containers/App.js
@@ -11,11 +11,12 @@ import JustifyContentBasics from '../components/JustifyContentBasics';
 import AlignItemsBasics from '../components/AlignItemsBasics';
 import PizzaTranslator from '../components/PizzaTranslator';
 import IScrolledDown from '../components/IScrolledDown';
+import ListViewBasics from '../components/ListViewBasics';
 
 export default class App extends Component {
   render() {
     return (
-      <IScrolledDown/>
+      <ListViewBasics/>
     );
   }
 }
